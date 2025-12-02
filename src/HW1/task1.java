93a13ea29ecf27682b5b1246f4de7cb8b9ee790b
prokop <prokopchik.jenya@gmail.com>
2025-12-02 23:42:31 +0300

    HomeWork5 in branch HW5

--- src/HW1/task1.java
+++ src/HW1/task1.java
@@ -2,7 +2,9 @@ package HW1;
 
 public class task1 {
     public static void main(String[] args){
-        int a,b,c;
+        int a;
+        int b;
+        int c;
         b=4;
         c=5;
         a=4*(b+c-1)/2;
